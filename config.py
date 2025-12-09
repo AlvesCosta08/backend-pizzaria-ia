e12cbf0ccca78f95efcd58c3b6c5e6a961d7ec9b
AlvesCosta <alvescosta74@gmail.com>
2025-12-09 13:11:13 -0300

    Backend pizzaria Ia

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'uma-chave-secreta-muito-forte-aqui')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Lav8@471')
     DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///pizzaria.db')
     API_BASE_URL = os.getenv('API_BASE_URL', 'http://backend:5000/api')
